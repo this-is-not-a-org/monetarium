d684cef1e73b384aaca6357fb2fd1be370267cca
markirius <markbombomb@gmail.com>
2021-03-28 05:09:10 -0300

    added coverage, decouple and dj_database_url

--- monetarium/settings.py
+++ monetarium/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+from decouple import Csv, config
+from dj_database_url import parse as db_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +23,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#i5_mz&bz!q81&=jxgze-pc#g^9xci)rh22$lr0x(6z3tp^w7)'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config(
+        "ALLOWED_HOSTS",
+        cast=Csv,
+        default="127.0.0.1, 0.0.0.0, localhost"
+    )
 
 
 # Application definition
@@ -54,7 +61,7 @@ ROOT_URLCONF = 'monetarium.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = config("LANGUAGE_CODE")
 
 TIME_ZONE = 'UTC'
 
