49cc8a9fb0840ce4465a2e6765607f784cd49308
lucaslima18 <lucas.ala1999@gmail.com>
2021-04-02 02:59:31 -0300

    adding the Planning model on admin site

--- apps/financial/admin.py
+++ apps/financial/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from . import models
 
+admin.site.register(models.Planning)
 admin.site.register(models.Card)
 admin.site.register(models.FinanceIncome)
 admin.site.register(models.Savings)
